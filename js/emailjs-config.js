a7a0084c5b4032f1f3a9f1056643d193321aa087
김로린 (Loreen Kim) <loreen@selectway.co.kr>
2026-02-10 11:49:45 +0900

    Updated .env.example

--- js/emailjs-config.js
+++ js/emailjs-config.js
@@ -8,8 +8,7 @@
 export const emailjsConfig = {
   publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || '',
   serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID || '',
-  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || '',
-  testEmail: import.meta.env.VITE_EMAILJS_TEST_EMAIL || ''
+  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || ''
 };
 
 export function hasEmailJSConfig() {
